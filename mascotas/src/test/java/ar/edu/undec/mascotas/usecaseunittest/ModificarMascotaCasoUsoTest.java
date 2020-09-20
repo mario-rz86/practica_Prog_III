816f73a61006fe90e06be107268c28c192f8838e
mario-rz86 <58262225+mario-rz86@users.noreply.github.com>
2020-09-20 16:01:38 -0300

    Se crea el Test de modificar mascota corrigiendo errores

--- mascotas/src/test/java/ar/edu/undec/mascotas/usecaseunittest/ModificarMascotaCasoUsoTest.java
+++ mascotas/src/test/java/ar/edu/undec/mascotas/usecaseunittest/ModificarMascotaCasoUsoTest.java
@@ -5,31 +5,34 @@ import ar.edu.undec.mascotas.casosUso.excepciones.ModificarMascotaException;
 import ar.edu.undec.mascotas.domain.Mascota;
 import ar.edu.undec.mascotas.repositorio.IConsultarMascotaPorNombreRepositorio;
 import ar.edu.undec.mascotas.repositorio.IModificarMascotaRepositorio;
+import mockito.MockitoExtension;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 
 import java.time.LocalDate;
 
 import static org.mockito.Mockito.when;
 
+@ExtendWith({MockitoExtension.class})
 public class ModificarMascotaCasoUsoTest {
 
     @Mock
-    IModificarMascotaRepositorio iModificarMascotaRepositorio;
+    IModificarMascotaRepositorio modificarMascotaRepositorio;
 
     @Mock
-    IConsultarMascotaPorNombreRepositorio iConsultarMascotaPorNombreRepositorio;
+    IConsultarMascotaPorNombreRepositorio consultarMascotaPorNombreRepositorio;
 
     @Test
     void modificarMascota_DatosCorrectos_mascotaModificada() throws ModificarMascotaException {
         Mascota mascotaAModificar = Mascota.instancia("batuque","pitbull", LocalDate.of(2019,6,15));
         Mascota mascotaDatosNuevos = Mascota.instancia("batuque","obejero belga", LocalDate.of(2019,6,15));
 
-        when(iConsultarMascotaPorNombreRepositorio.findByNombre("batuque")).thenReturn(mascotaAModificar);
-        when(iModificarMascotaRepositorio.update(mascotaAModificar)).thenReturn(true);
+        when(consultarMascotaPorNombreRepositorio.findByNombre("batuque")).thenReturn(mascotaAModificar);
+        when(modificarMascotaRepositorio.update(mascotaAModificar)).thenReturn(true);
 
-        ModificarMascotaCasoUso modificarMascotaCasoUso = new ModificarMascotaCasoUso(iModificarMascotaRepositorio, iConsultarMascotaPorNombreRepositorio);
+        ModificarMascotaCasoUso modificarMascotaCasoUso = new ModificarMascotaCasoUso(modificarMascotaRepositorio, consultarMascotaPorNombreRepositorio);
 
         Mascota mascota = modificarMascotaCasoUso.modificarMascota(mascotaDatosNuevos);
         Assertions.assertEquals("obejero belga",mascota.getRaza());
